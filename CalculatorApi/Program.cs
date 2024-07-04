9c336f99920728209ae4b0064eded91e3dc0999c
EssamSaed <162341217+EssamSaed@users.noreply.github.com>
2024-07-05 02:48:35 +0300

    Add files via upload

--- CalculatorApi/Program.cs
+++ CalculatorApi/Program.cs
@@ -1,33 +1,32 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
 var app = builder.Build();
 
+app.UseCors("AllowAll");
 
-app.MapGet("/", () => "Welcomen Herr Marcel, Ich bin Calculator API!");
+app.MapGet("/", () => "Welcome to Calculator API!");
 
-// Funktion zum Sammeln von Zahlen
+app.MapGet("/add", (double a, double b) => Results.Ok(a + b));
 
-app.MapGet("/add", (double a, double b) => 
-Results.Ok(a + b));
+app.MapGet("/subtract", (double a, double b) => Results.Ok(a - b));
 
-//Funktion zum Subtrahieren von Zahlen
+app.MapGet("/multiply", (double a, double b) => Results.Ok(a * b));
 
-app.MapGet("/subtract", (double a, double b) =>
-Results.Ok(a - b));
-
-//Funktion zum Multiplizieren von Zahlen
-app.MapGet("/multiply", (double a, double b) =>
-Results.Ok(a * b));
-
-//Funktion zum Teilen durch eine beliebige Zahl außer Null
 app.MapGet("/divide", (double a, double b) =>
 {
     if (b == 0)
     {
-        return Results.BadRequest("Eine Zahl kann nicht durch Null geteilt werden !");
+        return Results.BadRequest("Division by zero is not allowed.");
     }
     return Results.Ok(a / b);
 });
